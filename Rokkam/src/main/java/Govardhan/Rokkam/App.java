73d69bf85d07edbf4dce9088d845e2d7774fc8c6
rokkamgovardhan <37025723+rokkamgovardhan@users.noreply.github.com>
2019-08-21 09:12:03 +0530

    updated

--- Rokkam/src/main/java/Govardhan/Rokkam/App.java
+++ Rokkam/src/main/java/Govardhan/Rokkam/App.java
@@ -12,4 +12,9 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
     }
+    
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
 }
